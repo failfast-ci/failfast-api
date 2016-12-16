36b3bfda226e82755684930351cb63545e7bf071
elsacargnello <elsa.cargnello@gmail.com>
2016-12-16 15:19:41 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ test_requirements = [
 
 setup(
     name='hub2lab-hook',
-    version='0.2.0',
+    version='0.2.1',
     description="hub2lab-hook",
     long_description=readme,
     author="Antoine Legrand",
