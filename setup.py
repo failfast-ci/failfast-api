bbd1b33aa65b3b17b88e627bc314798786d80432
Antoine Legrand <2t.antoine@gmail.com>
2018-08-28 13:33:20 +0200

    Bump version: 0.4.1 → 0.5.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 setup(
     name='hub2lab-hook',
-    version='0.4.1',
+    version='0.5.0',
     description="hub2lab-hook",
     long_description=readme,
     author="Antoine Legrand",
