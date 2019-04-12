76566823e12da736887c39cdd936b83509d5e8a9
Maxime Guyot <maxime.guyot@elits.com>
2019-04-12 20:49:26 +0200

    Add error information in GitHub error

--- hub2labhook/jobs/tasks.py
+++ hub2labhook/jobs/tasks.py
@@ -106,7 +106,8 @@ def update_github_statuses_failure(self, request, exc, traceback, event,
     body = dict(
         state=GITHUB_STATUS_MAP["canceled"],
         target_url=(gevent.commit_url),  # TODO: link the gitlab YAML
-        description="An error occurred in initial pipeline execution",
+        description="An error occurred in pipeline execution. %s: %s" % (exc,
+            exc.message),
         context="%s/%s" % (FFCONFIG.github['context'], "pipeline"))
     return githubclient.post_status(body, gevent.repo, gevent.head_sha)
 
