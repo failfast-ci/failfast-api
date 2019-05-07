c63a86e3e86abdf68013a3859af8eed91cece553
ant31 <2t.antoine@gmail.com>
2019-05-07 14:57:39 +0200

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 setup(
     name='hub2lab-hook',
-    version='0.6.0',
+    version='0.6.1',
     description="hub2lab-hook",
     long_description=readme,
     author="Antoine Legrand",
